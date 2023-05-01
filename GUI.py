c876572d6f690f5e393421244e2563f5fabb3970
Robel Yonas <robel4872@gmail.com>
2023-05-01 22:32:43 +0200

    I have been able to implement emoji

--- GUI.py
+++ GUI.py
@@ -9,18 +9,8 @@ emotions = Emotions()
 pygame.mixer.init()
 
 # define button functions
-def happy():
-    quote = emotions.get_quote("happy")
-    quote_label.config(text=quote)
-    play_audio()
-
-def ok():
-    quote = emotions.get_quote("ok")
-    quote_label.config(text=quote)
-    play_audio()
-
-def sad():
-    quote = emotions.get_quote("sad")
+def update_emotion(emotion_name):
+    quote = emotions.get_quote(emotion_name.lower())
     quote_label.config(text=quote)
     play_audio()
 
@@ -41,25 +31,25 @@ root.geometry("500x550")
 root.resizable(False, False)
 
 # create canvas
-canvas = tk.Canvas(root, width=500, height=550, bg="#ffffff")
+canvas = tk.Canvas(root, width=500, height=550, bg="#F4D03F")
 canvas.pack()
 
-# create emotion buttons
-happy_btn = tk.Button(canvas, text="Happy", bg="#77dd77", fg="#ffffff", font=("Helvetica", 16), padx=20, pady=10, command=happy)
-ok_btn = tk.Button(canvas, text="Ok", bg="#ffa500", fg="#ffffff", font=("Helvetica", 16), padx=25, pady=10, command=ok)
-sad_btn = tk.Button(canvas, text="Sad", bg="#ff6961", fg="#ffffff", font=("Helvetica", 16), padx=25, pady=10, command=sad)
+# create emoji buttons
+happy_btn = tk.Button(canvas, text="😊", bg="#F4D03F", font=("Helvetica", 36), padx=20, pady=10, command=lambda: update_emotion("Happy"))
+ok_btn = tk.Button(canvas, text="😐", bg="#F4D03F", font=("Helvetica", 36), padx=20, pady=10, command=lambda: update_emotion("Ok"))
+sad_btn = tk.Button(canvas, text="😢", bg="#F4D03F", font=("Helvetica", 36), padx=20, pady=10, command=lambda: update_emotion("Sad"))
 
 # create audio buttons
-play_btn = tk.Button(canvas, text="Play", bg="#77dd77", fg="#ffffff", font=("Helvetica", 16), padx=20, pady=10, command=play_audio)
-pause_btn = tk.Button(canvas, text="Pause", bg="#ffa500", fg="#ffffff", font=("Helvetica", 16), padx=25, pady=10, command=pause_audio)
+play_btn = tk.Button(canvas, text="Play", bg="#0099cc", fg="#ffffff", font=("Helvetica", 16), padx=20, pady=10, command=play_audio)
+pause_btn = tk.Button(canvas, text="Pause", bg="#0099cc", fg="#ffffff", font=("Helvetica", 16), padx=25, pady=10, command=pause_audio)
 
 # create quote label
-quote_label = tk.Label(canvas, text="", font=("Helvetica", 16), anchor="center", justify="center", wraplength=400)
+quote_label = tk.Label(canvas, text="", font=("Helvetica", 16), anchor="center", justify="center", wraplength=400, fg="#ffffff", bg="#F4D03F")
 
 # add buttons and quote label to canvas
-canvas.create_window(250, 100, window=happy_btn)
-canvas.create_window(250, 200, window=ok_btn)
-canvas.create_window(250, 300, window=sad_btn)
+canvas.create_window(125, 250, window=happy_btn)
+canvas.create_window(250, 250, window=ok_btn)
+canvas.create_window(375, 250, window=sad_btn)
 canvas.create_window(125, 400, window=play_btn)
 canvas.create_window(375, 400, window=pause_btn)
 canvas.create_window(250, 475, window=quote_label)
