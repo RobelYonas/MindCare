63ecd7b18e5cf67840fed242cde7b021d449b640
Robel Yonas <robel4872@gmail.com>
2023-05-02 09:51:18 +0200

    some changes in color and window

--- GUI.py
+++ GUI.py
@@ -12,7 +12,6 @@ pygame.mixer.init()
 def update_emotion(emotion_name):
     quote = emotions.get_quote(emotion_name.lower())
     quote_label.config(text=quote)
-    play_audio()
 
 def play_audio():
     if not pygame.mixer.music.get_busy():
@@ -21,37 +20,52 @@ def play_audio():
     else:
         pygame.mixer.music.unpause()
 
-def pause_audio():
-    pygame.mixer.music.pause()
+def open_meditate_window():
+    # create new window for meditation
+    meditate_window = tk.Toplevel(root)
+    meditate_window.title("Meditation")
+    meditate_window.geometry("500x500")
+    meditate_window.resizable(False, False)
+
+    # create canvas
+    canvas = tk.Canvas(meditate_window, width=500, height=500, bg="#2C3E50")
+    canvas.pack()
+
+    # create label for meditation instructions
+    instructions_label = tk.Label(canvas, text="Close your eyes and focus on your breath.", font=("Helvetica", 16), anchor="center", justify="center", wraplength=400, fg="#ffffff", bg="#2C3E50")
+    canvas.create_window(250, 250, window=instructions_label)
+
+    # create button to play audio
+    play_btn = tk.Button(canvas, text="Play", bg="#3498DB", fg="#ffffff", font=("Helvetica", 16), padx=20, pady=10, command=play_audio)
+    canvas.create_window(250, 400, window=play_btn)
 
 # create main window
 root = tk.Tk()
 root.title("MindCare")
 root.geometry("500x550")
 root.resizable(False, False)
+root.config(bg="#2C3E50")
 
 # create canvas
-canvas = tk.Canvas(root, width=500, height=550, bg="#F4D03F")
+canvas = tk.Canvas(root, width=500, height=550, bg="#2C3E50")
 canvas.pack()
 
 # create emoji buttons
-happy_btn = tk.Button(canvas, text="😊", bg="#F4D03F", font=("Helvetica", 36), padx=20, pady=10, command=lambda: update_emotion("Happy"))
-ok_btn = tk.Button(canvas, text="😐", bg="#F4D03F", font=("Helvetica", 36), padx=20, pady=10, command=lambda: update_emotion("Ok"))
-sad_btn = tk.Button(canvas, text="😢", bg="#F4D03F", font=("Helvetica", 36), padx=20, pady=10, command=lambda: update_emotion("Sad"))
+happy_btn = tk.Button(canvas, text="😊", bg="#2C3E50", font=("Helvetica", 36), padx=20, pady=10, command=lambda: update_emotion("Happy"))
+ok_btn = tk.Button(canvas, text="😐", bg="#2C3E50", font=("Helvetica", 36), padx=20, pady=10, command=lambda: update_emotion("Ok"))
+sad_btn = tk.Button(canvas, text="😢", bg="#2C3E50", font=("Helvetica", 36), padx=20, pady=10, command=lambda: update_emotion("Sad"))
 
-# create audio buttons
-play_btn = tk.Button(canvas, text="Play", bg="#0099cc", fg="#ffffff", font=("Helvetica", 16), padx=20, pady=10, command=play_audio)
-pause_btn = tk.Button(canvas, text="Pause", bg="#0099cc", fg="#ffffff", font=("Helvetica", 16), padx=25, pady=10, command=pause_audio)
+# create meditate button
+meditate_btn = tk.Button(canvas, text="Meditate", bg="#3498DB", fg="#ffffff", font=("Helvetica", 16), padx=20, pady=10, command=open_meditate_window)
 
 # create quote label
-quote_label = tk.Label(canvas, text="", font=("Helvetica", 16), anchor="center", justify="center", wraplength=400, fg="#ffffff", bg="#F4D03F")
+quote_label = tk.Label(canvas, text="", font=("Helvetica", 16), anchor="center", justify="center", wraplength=400, fg="#ffffff", bg="#2C3E50")
 
 # add buttons and quote label to canvas
 canvas.create_window(125, 250, window=happy_btn)
 canvas.create_window(250, 250, window=ok_btn)
 canvas.create_window(375, 250, window=sad_btn)
-canvas.create_window(125, 400, window=play_btn)
-canvas.create_window(375, 400, window=pause_btn)
+canvas.create_window(250, 400, window=meditate_btn)
 canvas.create_window(250, 475, window=quote_label)
 
 # run the GUI application
